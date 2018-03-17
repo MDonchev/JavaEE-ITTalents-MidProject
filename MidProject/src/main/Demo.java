b0e22fb7c6f1d4c6e62013de1cc1244883b610a8
Mario Donchev <mariodonchew@gmail.com>
2018-03-17 16:51:35 +0200

    Testing new validation

--- MidProject/src/main/Demo.java
+++ MidProject/src/main/Demo.java
@@ -11,29 +11,32 @@ import users.Admin;
 import users.Guest;
 import users.Member;
 import users.User;
+import validate.Validation;
 
 public class Demo {
 	
 	public static Map<String, User> users = new HashMap<>();
 	public static Map<Product, Integer> availableProducts = new HashMap<>();
+	
 	public static final User guest = new Guest("Guest");
 	public static User currentUser = Demo.guest;
+	
+	
 	public static void main(String[] args) {
 		
 		currentUser.addProductToCatalog(new Hookah("Rashad", 124.5, HookahSize.MEDIUM), 3);
 		currentUser.addToCart(new Hookah("reer", 124.5, HookahSize.MEDIUM), 5);
 		currentUser.login("qwe@abv.bg", "123456");
-		register("araee", "123456", "ul.2", "qwe@abv.bg", "0883475671", false);
-		currentUser.login("qwe@abv.bg", "123456");
+		register("araee", "M123-aZ", "ul.2", "qwe@abv.bg", "0883475671", false);
+		currentUser.login("qwe@abv.bg", "M123-aZ");
 		currentUser.addToCart(new Hookah("reer", 124.5, HookahSize.MEDIUM), 1);
-		register("eaaw", "qwerty", "smth.2", "admin@admin.com", "0812345678", true);
-		currentUser.login("admin@admin.com", "qwerty");
 		currentUser.logout();
-		currentUser.login("admin@admin.com", "qwerty");
+		register("eaaw", "M123-aZ", "smth.2", "admin@admin.com", "0812345678", true);
+		currentUser.login("admin@admin.com", "M123-aZ");
 		currentUser.addProductToCatalog(new Hookah("Rashad", 124.5, HookahSize.MEDIUM), 3);
 		currentUser.addProductToCatalog(new Hookah("Abbot", 300.5, HookahSize.LARGE), 5);
 		currentUser.logout();
-		currentUser.login("qwe@abv.bg", "123456");
+		currentUser.login("qwe@abv.bg", "M123-aZ");
 		currentUser.addToCart(new Hookah("Rashad", 124.5, HookahSize.MEDIUM), 1);
 		currentUser.addToCart(new Hookah("Abbot", 300.5, HookahSize.LARGE), 3);
 		currentUser.makeOrder();
@@ -43,14 +46,13 @@ public class Demo {
 		currentUser.addToCart(new Hookah("Rashad", 124.5, HookahSize.MEDIUM), 1);
 		currentUser.login("qwe@abv.bg", "123456");
 		currentUser.logout();
-		System.out.println(currentUser.getCart());
+
 	}
 	
-	static void register(String name, String password, String address, String email, String number, boolean isAdmin) {
-		// TODO : make it better; almost 1 A.M. and so sleepy -> making bullshits
+	private static void register(String name, String password, String address, String email, String number, boolean isAdmin) {
+		
 		if (validate(name,password,address,email,number)) {
 			if (!users.keySet().contains(email)) {
-				//users.add(u);
 				if (isAdmin) {
 					users.put(email, new Admin(name, address, email, password, number));
 				}
@@ -68,11 +70,10 @@ public class Demo {
 	}
 
 	private static boolean validate(String name, String password, String address, String email, String number) {
-		if (name.equals(null) || password.equals(null) || address.equals(null) || email.equals(null) || number.equals(null)) return false;
-		if (!number.matches("08[0-9]{8}")) return false;
-		if (!email.matches("[\\w|.|-]*@\\w*\\.[\\w|.]+")) return false;
-		if (password.length() < 5) return false;
-		
-		return true;
+		return Validation.usernameValidation(name) &&
+				Validation.passwordValidation(password) &&
+				Validation.stringValidation(address) &&
+				Validation.mailValidation(email) &&
+				Validation.numberValidation(number);
 	}
 }
