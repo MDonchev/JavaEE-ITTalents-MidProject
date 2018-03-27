01ad9e2e2ce46faf37eb137c446e70dc900db0e6
Rangel Angelov <rangelangelov@gmail.com>
2018-03-27 21:52:47 +0300

    Empty manager classes

---
+++ MidProject/src/controller/manager/ProductManager.java
@@ -0,0 +1,13 @@
+package controller.manager;
+
+import model.Product;
+
+public class ProductManager {
+
+	private Product model;
+	
+	public ProductManager(Product model) {
+		this.model = model;
+	}
+	
+}
---
+++ MidProject/src/controller/manager/UserManager.java
@@ -0,0 +1,13 @@
+package controller.manager;
+
+import model.User;
+
+public class UserManager {
+
+	private User model;
+	
+	public UserManager(User model) {
+		this.model = model;
+	}
+	
+}
