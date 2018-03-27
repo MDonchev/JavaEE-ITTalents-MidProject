a3fdbac8a9032526a3249da6559a9fd21514feab
Rangel Angelov <rangelangelov@gmail.com>
2018-03-27 21:47:29 +0300

    The actual logic for Order is now in a seperate manager class

--- MidProject/src/model/Order.java
+++ MidProject/src/model/Order.java
@@ -1,11 +1,8 @@
-package order;
+package model;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-
-import products.Product;
-import users.User;
 
 public class Order {
 
@@ -18,22 +15,30 @@ public class Order {
 		this.dateAndTimeOfOrder = LocalDateTime.now();
 		this.orderedProducts = new HashMap();
 	}
-	
-	public void addProduct(Product product, int count) {
-		if(!this.orderedProducts.containsKey(product)) {
-			this.orderedProducts.put(product, 0);
-		}
-		
-		this.orderedProducts.put(product, this.orderedProducts.get(product) + count);
-	}
-	
-	public void finalizeOrder() {
-		this.dateAndTimeOfOrder = LocalDateTime.now();
+
+	public void setDateAndTimeOfOrder(LocalDateTime dateAndTimeOfOrder) {
+		this.dateAndTimeOfOrder = dateAndTimeOfOrder;
 	}
 
-	@Override
-	public String toString() {
-		return this.orderedProducts.toString() + " Date and time: " + this.dateAndTimeOfOrder;
+	public Map<Product, Integer> getOrderedProducts() {
+		return orderedProducts;
 	}
+	
+//	public void addProduct(Product product, int count) {
+//		if(!this.orderedProducts.containsKey(product)) {
+//			this.orderedProducts.put(product, 0);
+//		}
+//		
+//		this.orderedProducts.put(product, this.orderedProducts.get(product) + count);
+//	}
+//	
+//	public void finalizeOrder() {
+//		this.dateAndTimeOfOrder = LocalDateTime.now();
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return this.orderedProducts.toString() + " Date and time: " + this.dateAndTimeOfOrder;
+//	}
 	
 }
