9959e671a4b5782e51ea9a79c2ea9cd342b3bf0e
Rangel Angelov <rangelangelov@gmail.com>
2018-03-27 21:55:29 +0300

    Moved the basic logic from Order to its manager class

---
+++ MidProject/src/controller/manager/OrderManager.java
@@ -0,0 +1,28 @@
+package controller.manager;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import model.Order;
+import model.Product;
+
+public class OrderManager {
+
+	private Order model;
+	
+	public OrderManager(Order model) {
+		this.model = model;
+	}
+	
+	public void addProductToOrder(Product product, int count) {
+		Map<Product, Integer> modelProducts = this.model.getOrderedProducts();
+		if(!modelProducts.containsKey(product)) {
+			modelProducts.put(product, 0);
+		}
+		modelProducts.put(product, modelProducts.get(product) + count);
+	}
+	
+	public void finalizeOrder() {
+		this.model.setDateAndTimeOfOrder(LocalDateTime.now());
+	}
+}
